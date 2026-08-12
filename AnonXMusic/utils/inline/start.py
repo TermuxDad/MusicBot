d7cdae5f88c8b175499899c387b994f4e69de9e7
TermuxDad <76565235+TermuxDad@users.noreply.github.com>
2026-08-12 18:14:45 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -13,6 +13,9 @@ def start_panel(_):
             ),   
         ],
         [
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
+        ],
+        [
             InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
         ],
     ]
