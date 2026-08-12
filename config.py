f60322e4052ab1f154fdd46c8796cf9bc60ef10d
msiddux-droid <msiddux-droid@users.noreply.github.com>
2026-08-12 16:54:52 +0530

    Restore original repository links

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SkyBotsDeveloper/VivaanXmusic2.0",
+    "https://github.com/xbitcode/music.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
