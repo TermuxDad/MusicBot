b0a9775fc12dec9b2ba1f00c3c3edfd38d1c1dfb
msiddux-droid <msiddux-droid@users.noreply.github.com>
2026-08-12 16:56:31 +0530

    Point repository links to TermuxDad MusicBot

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SkyBotsDeveloper/VivaanXmusic2.0",
+    "https://github.com/TermuxDad/MusicBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
