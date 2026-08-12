665b3f9b0e54ec64cbef898104c3a1c3f78d0d1b
TermuxDad <76565235+TermuxDad@users.noreply.github.com>
2026-08-12 18:01:43 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -6,11 +6,14 @@ from AnonXMusic import app
 
 def start_panel(_):
     buttons = [
+        
         [
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
-            ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            ),   
+        ],
+        [
+            InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
         ],
     ]
     return buttons
@@ -24,16 +27,8 @@ def private_panel(_):
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_6"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_5"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
         ],
-        [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-        ],
-        [InlineKeyboardButton(text="• sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ •", url=f"https://github.com/TermuxDad/MusicBot")],
-    
     ]
-    
     return buttons
