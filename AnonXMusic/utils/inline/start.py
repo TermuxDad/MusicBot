60ef5d0f4ac29735e406dee0869dbe1150d87555
TermuxDad <76565235+TermuxDad@users.noreply.github.com>
2026-08-12 18:33:17 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -6,17 +6,10 @@ from AnonXMusic import app
 
 def start_panel(_):
     buttons = [
-        
         [
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
-            ),   
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
-        ],
-        [
-            InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
+            ),
         ],
     ]
     return buttons
@@ -31,7 +24,10 @@ def private_panel(_):
             )
         ],
         [
-            InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
+            InlineKeyboardButton(
+                text=_["S_B_4"], callback_data="settings_back_helper"
+            )
         ],
     ]
+
     return buttons
