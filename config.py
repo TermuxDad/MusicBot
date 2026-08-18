b8f9c55575485184a8dc169f17bdb2d55304e487
TermuxDad <76565235+TermuxDad@users.noreply.github.com>
2026-08-18 07:08:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -51,7 +51,7 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ArchonNetwork")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ArchonCare")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", false))
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
 ASSISTANT_LEAVE_TIME = int(getenv("ASSISTANT_LEAVE_TIME",  5400))
 
 
